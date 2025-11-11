9ede0a8a5c2e2a72c6c8b686390d70ac2f45f0e9
miguel nguetsa <fomekongnguetsa@gmail.com>
2025-11-11 23:37:13 +0100

    Ajout de la méthode factoriel

--- src/main/java/com/stjean/OperationMathematique.java
+++ src/main/java/com/stjean/OperationMathematique.java
@@ -4,4 +4,12 @@ public class OperationMathematique {
     public boolean estPositif(int nombre) {
         return nombre > 0;
     }
+    public int factoriel(int nombre) {
+        if (nombre < 0) return -1; // Pour l’instant, on retourne -1 si négatif
+        int result = 1;
+        for (int i = 1; i <= nombre; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
