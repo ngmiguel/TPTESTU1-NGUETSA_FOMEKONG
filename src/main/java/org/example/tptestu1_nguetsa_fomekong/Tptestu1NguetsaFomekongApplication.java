24b9cfafe01b86ebdb163f15e2e6494afa6d5f8e
miguel nguetsa <fomekongnguetsa@gmail.com>
2025-11-11 20:54:12 +0100

    initialisation du projet

---
+++ src/main/java/org/example/tptestu1_nguetsa_fomekong/Tptestu1NguetsaFomekongApplication.java
@@ -0,0 +1,13 @@
+package org.example.tptestu1_nguetsa_fomekong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tptestu1NguetsaFomekongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tptestu1NguetsaFomekongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tptestu1_nguetsa_fomekong/Tptestu1NguetsaFomekongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tptestu1_nguetsa_fomekong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tptestu1NguetsaFomekongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
