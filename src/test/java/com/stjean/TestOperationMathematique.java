76ab5890959f887db60c606b162205f1f28a9b5c
miguel nguetsa <fomekongnguetsa@gmail.com>
2025-11-12 00:55:41 +0100

    test de la méthode trier

--- src/test/java/com/stjean/TestOperationMathematique.java
+++ src/test/java/com/stjean/TestOperationMathematique.java
@@ -18,4 +18,16 @@ public class TestOperationMathematique {
         assertEquals(120, op.factoriel(5));
         assertEquals(-1, op.factoriel(-3)); // Pour l’instant, on teste avec -1
     }
+
+    @Test
+    public void testTrier() {
+        OperationMathematique op = new OperationMathematique();
+        int[] tableau = {3, 1, 4, 1, 5};
+        int[] resultat = op.trier(tableau);
+        assertEquals(5, resultat[0]);
+        assertEquals(4, resultat[1]);
+        assertEquals(3, resultat[2]);
+        assertEquals(1, resultat[3]);
+        assertEquals(1, resultat[4]);
+    }
 }
