ff9a846d5f9c22bc69865512dce17e723ad34056
miguel nguetsa <fomekongnguetsa@gmail.com>
2025-11-12 01:25:47 +0100

    ajout de la methode maxnumba

--- src/main/java/com/stjean/OperationMathematique.java
+++ src/main/java/com/stjean/OperationMathematique.java
@@ -25,4 +25,12 @@ public class OperationMathematique {
         }
         return resultat;
     }
+    public double maxNumba(double[] valeurs) {
+        if (valeurs.length == 0) return 0;
+        double max = valeurs[0];
+        for (double v : valeurs) {
+            if (v > max) max = v;
+        }
+        return max;
+    }
 }
